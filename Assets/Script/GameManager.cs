8ca5cf9bad75c3c8d352e4ccad0f3c3f440210b6
LeaSuLinHabel <lea.habel@yahoo.de>
2020-12-04 10:48:33 +0100

    UI, menu. texture + materials missing

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public void EnterLevel()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+/*
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+*/
+    
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+    }
+}
