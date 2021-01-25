3bcf2f0eed7bf35a34756830c436a4ae32b0ca96
guneyguner <guner444@gmail.com>
2021-01-25 23:44:46 +0100

    pups

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using Debug = UnityEngine.Debug;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class PlayerController : MonoBehaviour
@@ -23,10 +24,14 @@ public class PlayerController : MonoBehaviour
     private Stopwatch m_stopwatch;
     
     [SerializeField] private GameObject text = null;
+    [SerializeField] private GameObject countdown = null;
+    [SerializeField] private GameObject timeText = null;
     public GameObject gameOverText;
     public GameObject winText;
-    
-    
+    public GameObject[] respawns;
+    public float timeLeft = 3.0f;
+    private bool countdownRunning = false;
+
 
     // Start is called before the first frame update
     private void Start()
@@ -34,7 +39,7 @@ public class PlayerController : MonoBehaviour
         m_playerRigidbody = GetComponent<Rigidbody>();
         m_collectablesTotalCount = m_collectablesCounter = GameObject.FindGameObjectsWithTag("Collectable").Length;
         m_stopwatch = Stopwatch.StartNew(); //equal to: m_stopwatch = new Stopwatch; Stopwatch.start();
-        
+        respawns = GameObject.FindGameObjectsWithTag("Enemy");
         
     }
 
@@ -51,7 +56,21 @@ public class PlayerController : MonoBehaviour
         m_movementX = input.x;
         m_movementY = input.y*-1;
     }
-    
+
+    void Update()
+    {
+        while (countdownRunning == true)
+        {
+            timeLeft -= Time.deltaTime;
+            int seconds = Convert.ToInt32( timeLeft % 60);
+            Debug.Log("Time Left:" + seconds);
+            timeText.GetComponent<Text>().text = $"{seconds}";
+            if (timeLeft < 0)
+            {
+                countdownRunning = false;
+            }
+        }
+    }
 
     private void FixedUpdate()
     {
@@ -61,59 +80,80 @@ public class PlayerController : MonoBehaviour
         //playerTransform.eulerAngles  = new Vector3(0.0f, 0.0f, 0.0f);
 
         //GameObject.Transform.rotation = Vector3(0.0f,0.0f,0.0f);
+       
     }
-
+    
     private void OnTriggerEnter(Collider other)
     { 
         Scene currentScene = SceneManager.GetActiveScene (); 
         string sceneName = currentScene.name;
-       if(other.gameObject.CompareTag("Collectable"))
-       {
-           other.gameObject.SetActive(false);
-           m_collectablesCounter--;
-           Debug.Log(m_collectablesCounter);
-           if (m_collectablesCounter == 0)
-           {
-               if (sceneName == "level1")
-               {
-                   SceneManager.LoadScene("level2");
-               }
-               if (sceneName == "level2")
-               {
-                   text.GetComponent<UnityEngine.UI.Text>().text = "YOU WIN!";
-                   winText.SetActive(true);
-                   //Debug.Log($"It took you {m_stopwatch.Elapsed} to find all {m_collectablesTotalCount} collectables. ");
-                   Invoke("endGame", 1);
-               }
-               
-           }
-           else
-           {
-               text.GetComponent<UnityEngine.UI.Text>().text = 
-                   $"SCORE: {m_collectablesTotalCount-m_collectablesCounter} / {m_collectablesTotalCount}";
-               //Debug.Log($"'You've already found {m_collectablesTotalCount - m_collectablesCounter} of {m_collectablesTotalCount} collectables!");
-           }
-       }
-       else if(other.gameObject.CompareTag("Enemy"))
-       {
-           text.GetComponent<UnityEngine.UI.Text>().text = "YOU GOT CAUGHT!";
-           gameOverText.SetActive(true);
-           Invoke("endGame", 1);
-       }
-       else if(other.gameObject.CompareTag("MovingObstacle"))
-       {
-           text.GetComponent<UnityEngine.UI.Text>().text = "YOU HIT AN OBSTACLE!";
-           //Time.timeScale = 0;
-           gameOverText.SetActive(true);
-           Invoke("endGame", 1);
-
-       }
+        if(other.gameObject.CompareTag("Collectable"))
+        {
+            other.gameObject.SetActive(false);
+            m_collectablesCounter--;
+            //Debug.Log(m_collectablesCounter);
+            if (m_collectablesCounter == 0)
+            {
+                if (sceneName == "level1")
+                {
+                    SceneManager.LoadScene("level2");
+                }
+                if (sceneName == "level2")
+                {
+                    text.GetComponent<UnityEngine.UI.Text>().text = "YOU WIN!";
+                    winText.SetActive(true);
+                    //Debug.Log($"It took you {m_stopwatch.Elapsed} to find all {m_collectablesTotalCount} collectables. ");
+                    Invoke("endGame", 2);
+                }
+            }
+            else
+            {
+                text.GetComponent<UnityEngine.UI.Text>().text = 
+                    $"SCORE: {m_collectablesTotalCount-m_collectablesCounter} / {m_collectablesTotalCount}";
+                //Debug.Log($"'You've already found {m_collectablesTotalCount - m_collectablesCounter} of {m_collectablesTotalCount} collectables!");
+            }
+        }
+        else if (other.gameObject.CompareTag("Invincible"))
+        {
+            other.gameObject.SetActive(false);
+            foreach (GameObject respawn in respawns)
+            {
+                respawn.SetActive(false);
+            }
+            countdownRunning = true;
+            Invoke("setFalse", 10);
+            timeText.SetActive(true);
+            countdown.SetActive(true);
+            countdown.GetComponent<Text>().text = $"Invincibility Countdown";
+        }
+        else if(other.gameObject.CompareTag("Enemy"))
+        {
+            text.GetComponent<UnityEngine.UI.Text>().text = "YOU GOT CAUGHT!";
+            gameOverText.SetActive(true);
+            Invoke("endGame", 2);
+        }
+        else if(other.gameObject.CompareTag("MovingObstacle"))
+        {
+            text.GetComponent<UnityEngine.UI.Text>().text = "YOU HIT AN OBSTACLE!";
+            //Time.timeScale = 0;
+            gameOverText.SetActive(true);
+            Invoke("endGame", 2);
+        }
        
     }
 
+    void setFalse()
+    {
+        foreach (GameObject respawn in respawns)
+        {
+            respawn.SetActive(true);
+        }
+        //Debug.Log("Hi");
+        countdown.SetActive(false);
+        timeText.SetActive(false);
+    }
     private void endGame()
     {
-        
         SceneManager.LoadScene("Menu");
     }
    
